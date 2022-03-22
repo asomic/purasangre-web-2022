ecd3284e2571b5caf1c772b9530a8196fe0257c1
dandelior <jberrioscifuentes@gmail.com>
2022-03-21 22:54:01 -0300

    Values added

---
+++ src/components/values.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/values.sass";
+
+const Values = () => {
+  return (
+    <section className="values">
+      <div className="values-inner"></div>
+    </section>
+  );
+};
+
+export default Values;
