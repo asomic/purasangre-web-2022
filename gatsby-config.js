eac60dfe9695710ac082215bb4a4c8d088380646
dandelior <jberrioscifuentes@gmail.com>
2022-03-29 22:02:49 -0300

    graphql plugin removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,16 +8,5 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
-    {
-      resolve: `gatsby-source-graphql`,
-      options: {
-        typeName: `GitHub`,
-        fieldName: `github`,
-        url: `https://admin.purasangrecrossfit.cl/api`,
-        // headers: {
-        //   Authorization: `Bearer your-github-token`,
-        // },
-      },
-    },
   ],
 };
