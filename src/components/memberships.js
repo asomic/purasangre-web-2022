abc2ae7604a2ef96ac2e3451d1ea8ec837514894
dandelior <jberrioscifuentes@gmail.com>
2022-04-06 16:30:57 -0400

    remove comment

--- src/components/memberships.js
+++ src/components/memberships.js
@@ -228,7 +228,6 @@ const Memberships = ({ summarized, title, moreLink }) => {
           ...getPersonalizado(),
         ];
         setMembershipsData(thePlans);
-        // setMemberships(resultData.plans);
       });
   }, []);
 
