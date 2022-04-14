c5afaee210435b102dd76deeb4678ce28b39f4d4
dandelior <jberrioscifuentes@gmail.com>
2022-04-14 16:24:25 -0400

    details

--- src/components/infoCards.js
+++ src/components/infoCards.js
@@ -3,7 +3,11 @@ import React from "react";
 import "../styles/InfoCards.sass";
 
 const Infocards = ({ children }) => {
-  return <div className="info-cards">{children}</div>;
+  return (
+    <div className="info-cards-wrapper">
+      <div className="info-cards">{children}</div>
+    </div>
+  );
 };
 
 export default Infocards;
